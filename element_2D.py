ae5b24f423fb58d20b22ef57f461737286c8d9c5
Max Mignolet <maxmignolet278@gmail.com>
2020-08-12 23:35:48 +0200

    2D boudnary element, not working

--- element_2D.py
+++ element_2D.py
@@ -11,46 +11,51 @@ import numpy as np
 hbar = 1.054e-16  # kg*mm^2/s
 m = 9.109e-31
 
+
 class LinearRectangleTriangle:
     """
     Triangle of height b and width a, with DOFs numbered as such :
-        
+
         3
         | \
         |  \
         1---2
         the first DOF must be the one with the right angle
         DOF 1 is at (x0, y0)
-        
+
         (Rotations should be OK)
     """
-    
+
     def __init__(self, x0, y0, a, b, ddls):
         self.ddls = ddls
         self.x0 = x0
         self.y0 = y0
         self.a = a
         self.b = b
-        
+
         self.K = None
         self.M = None
-        
-        
+
     # TODO : mutualiser l'interface des éléments
+
     def get_stiffness(self):
         if self.K is None:
             self.compute_stiffness()
         return self.K
 
     def compute_mass(self):
-        self.M = self.a * self.b / 12 * np.matrix([[1, 1/2, 1/2], [1/2, 1, 1/2], [1/2, 1/2, 1]])
-        
+        self.M = self.a * self.b / 12 * np.matrix([
+            [1, 1/2, 1/2],
+            [1/2, 1, 1/2],
+            [1/2, 1/2, 1]
+            ])
+
     def compute_stiffness(self):
         a = self.a
         b = self.b
         self.K = 0.5 * np.matrix([
-                [a/b + b/a, -b/a, -a/b], 
-                [-b/a, b/a, 0], 
+                [a/b+b/a, -b/a, -a/b],
+                [-b/a, b/a, 0],
                 [-a/b, 0, a/b]
                 ])
 
@@ -65,25 +70,31 @@ class LinearRectangleTriangle:
             raise ValueError("Element has unimplemented potential")
         return (hbar*hbar/(2*m) * self.get_stiffness()
                 + self.V0 * self.get_mass())
-        
+
+
 class RectangleElement:
     """
     Rectangle of width a and height b with origin (DOF 1) at x0, y0.
     Made of two CSTs paired together
+
+    3---4
+    |   |
+    |   |
+    1---2
     """
-    
+
     def __init__(self, x0, y0, a, b, ddls):
         self.ddls = ddls
         self.x0 = x0
         self.y0 = y0
         self.a = a
         self.b = b
-        
+
         self.K = None
         self.M = None
-        
-        
+
     # TODO : mutualiser l'interface des éléments
+
     def get_stiffness(self):
         if self.K is None:
             self.compute_stiffness()
@@ -91,21 +102,21 @@ class RectangleElement:
 
     def compute_mass(self):
         self.M = self.a * self.b / 12 * np.matrix([
-                [1, 1/2, 1/2, 0], 
-                [1/2, 2, 1, 1/2], 
+                [1, 1/2, 1/2, 0],
+                [1/2, 2, 1, 1/2],
                 [1/2, 1, 2, 1/2],
                 [0, 1/2, 1/2, 1]
                 ])
-        
+
     def compute_stiffness(self):
         a = self.a
         b = self.b
         self.K = 0.5 * np.matrix([
-                [a/b + b/a, -b/a, -a/b, 0],
-                [-b/a, b/a+a/b, 0, -b/a],
+                [a/b+b/a, -b/a, -a/b, 0],
+                [-b/a, a/b+b/a, 0, -b/a],
                 [-a/b, 0, a/b+b/a, -a/b],
                 [0, -b/a, -a/b, a/b+b/a]
-            ])
+                ])
 
     def get_mass(self):
         if self.M is None:
@@ -118,3 +129,58 @@ class RectangleElement:
             raise ValueError("Element has unimplemented potential")
         return (hbar*hbar/(2*m) * self.get_stiffness()
                 + self.V0 * self.get_mass())
+
+
+class RectangleElementBoundary(RectangleElement):
+
+    """
+    Types:
+        0: corner
+        1: vertical border
+        2: horizontal border
+        3: inner corner
+               3
+               |
+               |
+            1--2
+    """
+    def __init__(self, x0, y0, a, b, ddls, elem_type):
+        super().__init__(x0, y0, a, b, ddls)
+        self.elem_type = elem_type
+
+    def compute_mass(self):
+        if self.elem_type == 0:
+            self.M = self.a * self.b / 12 * np.matrix([1])
+        elif self.elem_type in  (1, 2):
+            self.M = self.a * self.b / 12 * np.matrix([
+                    [1, 1/2],
+                    [1/2, 2],
+                    ])
+        elif self.elem_type == 3:
+            self.M = self.a * self.b / 12 * np.matrix([
+                    [1, 1/2, 0],
+                    [1/2, 2, 1/2],
+                    [0, 1/2, 1]
+                    ])
+
+    def compute_stiffness(self):
+        a = self.a
+        b = self.b
+        if self.elem_type == 0:
+            self.K = 0.5 * np.matrix([a/b])
+        elif self.elem_type == 1:
+            self.K = 0.5 * np.matrix([
+                    [a/b+b/a, -a/b],
+                    [-a/b, a/b+b/a],
+                    ])
+        elif self.elem_type == 2:
+            self.K = 0.5 * np.matrix([
+                    [a/b+b/a, -b/a],
+                    [-b/a, a/b+b/a]
+                    ])
+        elif self.elem_type == 3:
+            self.K = 0.5 * np.matrix([
+                    [a/b+b/a, -b/a, 0],
+                    [-b/a, a/b+b/a, -b/a],
+                    [0, -b/a, a/b+b/a]
+                    ])
--- infinite_well_2D.py
+++ infinite_well_2D.py
@@ -5,12 +5,13 @@ Created on Mon Aug 10 23:09:02 2020
 @author: boris
 """
 
-#Parameters
+
 from abstract_mesh import AbstractMesh
-from element_2D import RectangleElement
+from element_2D import RectangleElement, RectangleElementBoundary
 import numpy as np
 from scipy.linalg import eigh
 
+# Parameters
 eV = 1.602e-1  # kg*nm^2/s^2
 hbar = 1.054e-16  # kg*mm^2/s
 nm = 1
@@ -22,6 +23,7 @@ TODO:
     improve computation time by preallocating?
 """
 
+
 class MeshPotentialWell2D(AbstractMesh):
 
     def __init__(self, w, h, nx, ny):
@@ -31,54 +33,105 @@ class MeshPotentialWell2D(AbstractMesh):
 
     def construct(self, w, h, nx, ny):
         """Precision = Element lenth in multiples of x_caract"""
-        self.dof_counter = (nx + 1) * (ny + 1) - 1 #Hardcoded :c
+        self.dof_counter = (nx - 1) * (ny - 1) - 1  # Hardcoded :c
         self.elements = []
 
-        
+        # bottom left corner
+        elem = RectangleElementBoundary(0, 0, w, h, [0], 0)
+        elem.V0 = 0
+        self.elements.append(elem)
+        # bottom right corner
+        elem = RectangleElementBoundary((nx-2) * w, 0, w, h, [nx-2], 0)
+        elem.V0 = 0
+        self.elements.append(elem)
+        # top left corner
+        elem = RectangleElementBoundary(0, (ny-2) * h, w, h, [(ny-2)*(nx-3)], 0)
+        elem.V0 = 0
+        self.elements.append(elem)
+        # top right corner
+        elem = RectangleElementBoundary((nx-2) * w, (ny-2) * h, w, h, [(ny-2)*(nx-2)], 0)
+        elem.V0 = 0
+        self.elements.append(elem)
 
-        for i in range(0, nx):
-            for j in range(0, ny):
+        # top and bottom borders
+        for i in range(1, nx-2):
+            x = i * w
+            j = 0
+            y = 0
+            elem = RectangleElementBoundary(x, y, w, h,
+                                            [i + j * (nx - 2),
+                                             i + (j + 1) * (nx - 2)],
+                                            2)
+            elem.V0 = 0
+            self.elements.append(elem)
+
+            # x = i * w
+            j = ny - 2
+            y = (ny - 2) * h
+            elem = RectangleElementBoundary(x, y, w, h,
+                                            [i + j * (nx - 2),
+                                             i + (j + 1) * (nx - 2)],
+                                            2)
+            elem.V0 = 0
+            self.elements.append(elem)
+
+        # left and right borders
+        for j in range(1, ny-2):
+            i = 0
+            x = 0
+            y = j * h
+            elem = RectangleElementBoundary(x, y, w, h,
+                                            [i + j * (nx - 2),
+                                             i + j * (nx - 2) + 1],
+                                            1)
+            elem.V0 = 0
+            self.elements.append(elem)
+
+            i = nx - 2
+            x = (nx - 2) * w
+            # y = j * h
+            elem = RectangleElementBoundary(x, y, w, h,
+                                            [i + j * (nx - 2),
+                                             i + j * (nx - 2) + 1],
+                                            1)
+            elem.V0 = 0
+            self.elements.append(elem)
+
+        # inner elements
+        for i in range(1, nx-2):
+            for j in range(1, ny-2):
                 x = i * w
                 y = j * h
-                
-                elem = RectangleElement(x, y, w, h, [i + j * (nx + 1),\
-                                                     i + j * (nx + 1) + 1,\
-                                                     i + (j + 1) * (nx + 1),\
-                                                     (i + 1) + (j + 1) * (nx + 1)])
+
+                elem = RectangleElement(x, y, w, h,
+                                        [i + j * (nx - 2),
+                                         i + j * (nx - 2) + 1,
+                                         i + (j + 1) * (nx - 2),
+                                         (i + 1) + (j + 1) * (nx - 2)])
                 elem.V0 = 0
                 self.elements.append(elem)
 
-
-        # Remove boundary DOFs
-        boundary_DOFs = [i for i in range(0, nx + 1)] + \
-                        [i + (ny * (nx + 1)) for i in range(0, nx + 1)] + \
-                        [j * (nx + 1) for j in range (0, ny + 1)] + \
-                        [j * (nx + 1) + nx for j in range (0, ny + 1)]
-        boundary_DOFs = list(dict.fromkeys(boundary_DOFs))
-        boundary_DOFs = sorted(boundary_DOFs, reverse = True)
-        print(boundary_DOFs)
         self.construct_matrices()
-        
-        for dof in boundary_DOFs:
-            self.remove_DOF(dof)
-        
-        
+
+
 if __name__ == "__main__":
     width = nm
     height = 1.5*nm
-    nx = 40 #Number of rectangles in x-axis
+    nx = 40  # Number of rectangles in x-axis
     ny = 60
-    
-    E0 = (hbar * np.pi) **2 / (2 * m)
+
+    E0 = (hbar * np.pi) ** 2 / (2 * m)
     E_1_1 = E0 * (1 / width**2 + 1 / height**2)
     print("Energie min : %2.3f eV" % (E_1_1 / eV))
-    expected_energies = sorted([E0 * (((x+1)/width)**2 + ((y+1)/height)**2) for x, y in np.ndindex(nx, ny)])
-    
+    expected_energies = sorted([E0 * (((x+1)/width)**2 + ((y+1)/height)**2)
+                               for x, y in np.ndindex(nx, ny)])
+
     # Element size
     w = width / nx
     h = height / ny
     mesh = MeshPotentialWell2D(w, h, nx, ny)
     eigvals, eigvecs = eigh(mesh.H, mesh.M, eigvals_only=False)
     for i, E in enumerate(sorted(eigvals)[0:20], start=1):
-        print("%d : %2.3f eV. Expected : %2.3f eV. Excess : %2.3f %%" % (i, E/eV, expected_energies[i-1]/eV, 100 * (E / expected_energies[i-1] - 1)))
-
+        print("%d : %2.3f eV. Expected : %2.3f eV. Excess : %2.3f %%" %
+              (i, E/eV, expected_energies[i-1]/eV,
+               100 * (E / expected_energies[i-1] - 1)))
